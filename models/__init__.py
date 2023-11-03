bd8f66b7f1b6844dde24e22eba795d7012646cfa
JayKarhade <jaykarhade@yahoo.co.in>
2023-11-03 17:33:09 -0400

    added drop point transformer

---
+++ models/__init__.py
@@ -0,0 +1,4 @@
+from .build import build_model_from_cfg
+import models.Point_MAE
+# import models.Drop_Point_MAE
+# import models.Point_JEPA
---
+++ tools/__init__.py
@@ -0,0 +1,8 @@
+# from .runner import run_net
+from .runner import test_net
+from .runner_pretrain import run_net as pretrain_run_net
+# from .runner_jepa_pretrain import jepa_run_net as pretrain_run_net
+from .runner_finetune import run_net as finetune_run_net
+# from .runner_jepa_finetune import run_net as finetune_run_net
+from .runner_finetune import test_net as test_run_net
+# from .runner_jepa_finetune import test_net as test_run_net
